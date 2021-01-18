5ccdcdc660844172dde20b9a44a4e36712b7f63f
wey <weybrian@gmail.com>
2021-01-18 17:24:55 +0800

    numberType add instanceof

--- practice/src/varibleValue/NumberType.java
+++ practice/src/varibleValue/NumberType.java
@@ -2,15 +2,19 @@ package varibleValue;
 
 public class NumberType {
 //想用0b轉數字失敗
-	
-	short num1 = 0b1011;//0b example
+
+	Short num1 = 0b10_11;// 0b example
+	Short num4 = 10;
+	int num3 = 11;
 	char[] array = new char[10];
 	int num2;
 	StringBuffer stringBuffer = new StringBuffer();
 	String str1;
-	
+
 	public void printNum1() {
-//		System.out.println(num1);
+		System.out.println(num1 instanceof Short);
+		System.out.println(num4 instanceof Short);
+		System.out.println(num1==num3);
 		for (int i = 0; i < array.length; i++) {
 			if (Math.random()>=0.5) {
 				array[i] = '1';				
@@ -25,9 +29,9 @@ public class NumberType {
 //		num2 = Integer.valueOf(str1);
 		System.out.println(num2);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		new NumberType().printNum1();
 
 	}
